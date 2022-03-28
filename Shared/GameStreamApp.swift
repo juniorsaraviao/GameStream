f6f55e86254e10287fd7cc035a11462fe3c1c122
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-27 23:08:40 -0500

    Initial Commit

---
+++ Shared/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  Shared
+//
+//  Created by Junior Eduardo Saravia Oria on 27/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
