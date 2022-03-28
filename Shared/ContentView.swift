3fe356b9486cb18802de4d089fdc850314723787
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-27 23:48:48 -0500

    Add first page

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Color(red: 22/255, green: 31/255, blue: 59/255, opacity: 1.0).ignoresSafeArea()
+            
+            VStack {
+                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250)
+                
+                InicioYRegistroView()
+            }
+        }
+    }
+}
+
+struct InicioYRegistroView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("INICIA SESIÓN")
+                
+                Text("REGÍSTRATE")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("pantalla1").resizable()
+        
         ContentView()
     }
 }
