f6be2fa8423e5a39eba0c423fb418d718046532f
Junior Saravia <junior.saravia.o@uni.pe>
2022-03-30 23:05:50 -0500

    create HomeScreen

--- Shared/Home.swift
+++ Shared/Home.swift
@@ -8,39 +8,65 @@
 import SwiftUI
 
 struct Home: View {
+    @State var selectedTab: Int = 2
+    
     var body: some View {
         
-        TabView {
+        TabView(selection: $selectedTab) {
             
             Text("Perfil")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "person")
                     Text("Perfil")
-                }
+                }.tag(0)
             
             Text("Juegos")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "gamecontroller")
                     Text("Juegos")
-                }
+                }.tag(1)
             
-            Text("Welcome to Home")
+            HomeScreen()
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "house")
                     Text("Inicio")
-                }
+                }.tag(2)
             
             Text("Favoritos")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "heart")
                     Text("Home")
-                }
-        }
+                }.tag(3)
+        }.accentColor(.white)
         
+    }
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color("TabBar-Color"))
+        
+        UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+        UITabBar.appearance().isTranslucent = true
+        
+        print("Init home view")
+    }
+}
+
+
+struct HomeScreen : View {
+    var body: some View {
+        ZStack {
+            Color("marine").ignoresSafeArea()
+            
+            VStack {
+                Text("Welcome!")
+            }.padding(.horizontal, 18)
+        }
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
