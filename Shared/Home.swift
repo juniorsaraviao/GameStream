29be7d6e41223411b81d79c61563ad1d5d988ad8
Junior Saravia <junior.saravia.o@uni.pe>
2022-04-01 00:00:32 -0500

    add carousel

--- Shared/Home.swift
+++ Shared/Home.swift
@@ -146,6 +146,98 @@ struct SubModuleHome : View {
                 
             }.frame(minWidth: 0, maxWidth: .infinity,alignment: .center)
                 .padding(.vertical)
+            
+            Text("CATEGORÍAS SUGERIDAS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    Button(action: {}) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("blueBot"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("FPS")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                    
+                    Button(action: {}) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("blueBot"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("RPG")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                    
+                    Button(action: {}) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("blueBot"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("open-world")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                }
+            }
+            
+            Text("RECOMENDADOS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    Button(action: {
+                        url = urlVideos[1]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }) {
+                        Image("13-swiftuiapps-2105-assassins_creed")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 240, height: 135)
+                    }
+                    
+                    Button(action: {
+                        url = urlVideos[2]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }) {
+                        Image("13-swiftuiapps-2105-battkefield")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 240, height: 135)
+                    }
+                    
+                    Button(action: {
+                        url = urlVideos[3]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }) {
+                        Image("13-swiftuiapps-2105-dest")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 240, height: 135)
+                    }
+                }
+            }
         }
         
         NavigationLink(destination: VideoPlayer(player: AVPlayer(url: URL(string: url)!)).frame(width: 400, height: 300), isActive: $isPlayerActive) {
