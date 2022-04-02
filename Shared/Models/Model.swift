d35a3f259ef8864935595eb941a2bd0c7f22d716
Junior Saravia <junior.saravia.o@uni.pe>
2022-04-01 23:44:16 -0500

    model Games

--- Shared/Models/Model.swift
+++ Shared/Models/Model.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Games: Codable {
+    let games: [Game]
+}
+
+struct Game: Codable {
+    let title, studio, contentRaiting, publicationYear: String
+    let welcomeDescription: String
+    let platforms, tags: [String]
+    let videosUrls: VideosUrl
+    let galleryImages: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case title, studio, contentRaiting, publicationYear
+        case welcomeDescription = "description"
+        case platforms, tags, videosUrls, galleryImages
+    }
+}
+
+struct VideosUrl: Codable {
+    let mobile, tablet: String
+}
+
+typealias Welcome = [Games]
