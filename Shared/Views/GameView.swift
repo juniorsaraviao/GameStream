89e872af3885f6cbb3d0764d7370b47641fc1294
Junior Saravia <junior.saravia.o@uni.pe>
2022-04-03 20:40:23 -0500

    add Gallery and Comments

--- Shared/Views/GameView.swift
+++ Shared/Views/GameView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AVKit
+import Kingfisher
 
 struct GameView: View {
     
@@ -33,6 +34,10 @@ struct GameView: View {
                     InfoVideo(title: title, studio: studio, calification: calification, publicationYear: publicationYear, description: description, tags: tags)
                         .padding(.bottom)
                     
+                    Gallery(galleryImages: galleryImages)
+                    
+                    Comments()
+                    
                 }.frame(maxWidth: .infinity)
             }
         }
@@ -55,8 +60,12 @@ struct Video: View {
     var url: String
     
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: URL(string: url)!))
+        let player = AVPlayer(url: URL(string: url)!)
+        VideoPlayer(player: player)
             .ignoresSafeArea()
+            .onDisappear(perform: {
+                player.pause()
+            })
     }
 }
 
@@ -115,5 +124,95 @@ struct InfoVideo: View {
             }
             
         }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct Gallery: View {
+    
+    var galleryImages: [String]
+    
+    let gridShape = [
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("GALERÍA")
+                .foregroundColor(.white)
+                .font(.title)
+                .padding(.leading)
+            
+            ScrollView(.horizontal) {
+                
+                LazyHGrid(rows: gridShape, spacing: 8) {
+                    ForEach(galleryImages, id: \.self){
+                        img in
+                        KFImage(URL(string: img))
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    }
+                }
+                
+            }.frame(height: 180)
+            
+        }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct Comments: View {
+    
+    var body: some View{
+        
+        VStack(alignment: .leading){
+            
+            Text("COMENTARIOS").foregroundColor(.white).font(.title).padding(.leading)
+            
+            VStack {
+                Spacer()
+                HStack{
+                    
+                    Image("perfilEjemplo").resizable().aspectRatio(contentMode: .fit).frame(width: 80, height: 80, alignment: .leading).padding(.leading)
+                    
+                    VStack{
+                        
+                        Text("Enrique Ramos").foregroundColor(.white).font(.subheadline).fontWeight(.bold).frame(alignment: .leading).padding(.bottom)
+                        
+                        Text("Hace 7 días").foregroundColor(.white).font(.subheadline).frame(alignment: .leading).padding(.bottom)
+                        
+                        
+                    }
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("He visto que como media tiene una gran calificación, y estoy completamente de acuerdo. Es el mejor juego que he jugado sin ninguna duda, combina una buena trama con una buenísima experiencia de juego libre gracias a su inmenso mapa y actividades.").foregroundColor(.white).font(.subheadline).frame(alignment: .leading)
+                    .padding(.all, 5)
+                Spacer()
+                
+            }.frame(maxWidth: .infinity, alignment: .leading).background(Color("blueBot")).clipShape(RoundedRectangle(cornerRadius: 8)).padding(.all, 10)
+            
+            VStack {
+                Spacer()
+                HStack{
+                    
+                    Image("perfilEjemplo").resizable().aspectRatio(contentMode: .fit).frame(width: 80, height: 80, alignment: .leading).padding(.leading)
+                    
+                    VStack{
+                        
+                        Text("Pamela Torres").foregroundColor(.white).font(.subheadline).fontWeight(.bold).frame(alignment: .leading).padding(.bottom)
+                        
+                        Text("Hace 12 días").foregroundColor(.white).font(.subheadline).frame(alignment: .leading).padding(.bottom)
+                        
+                        
+                    }
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("He visto que como media tiene una gran calificación, y estoy completamente de acuerdo. Es el mejor juego que he jugado sin ninguna duda, combina una buena trama con una buenísima experiencia de juego libre gracias a su inmenso mapa y actividades.").foregroundColor(.white).font(.subheadline).frame(alignment: .leading)
+                
+                Spacer()
+                
+            }.frame(maxWidth: .infinity, alignment: .leading).background(Color("blueBot")).clipShape(RoundedRectangle(cornerRadius: 8)).padding(.all, 10)
+            
+        }
     }
 }
