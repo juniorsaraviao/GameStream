992c63387a12f8fa85b8c6c0b1b6ae360027e5c2
Junior Saravia <junior.saravia.o@uni.pe>
2022-04-03 19:31:32 -0500

    add default data

--- Shared/Views/GameView.swift
+++ Shared/Views/GameView.swift
@@ -20,12 +20,23 @@ struct GameView: View {
     var galleryImages: [String]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("marine").ignoresSafeArea()
+            
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
-//struct GameView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        GameView()
-//    }
-//}
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView(url: "ejemplo",title:"Sonic The Hedgehog"
+            ,studio:"Sega"
+            ,calification:"E+"
+            ,publicationYear:"1991"
+            ,description:"Juego de Sega Genesis publicado en 1991 con más de 40 millones de copias vendidas actualmente"
+            ,tags:["plataformas","mascota"], galleryImages: [ "https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg"])
+    }
+}
